5ce30978768947f43e0256443cf1235979adef7c
sanrre <shavibimi@gmail.com>
2020-09-22 22:15:11 +0400

    scripts

---
+++ scripts/watch.py
@@ -0,0 +1,48 @@
+import os
+import sys
+import time
+import subprocess
+from threading import Thread
+
+fn = sys.argv[1]
+
+c_file = ""
+
+def compile_and_run():
+	os.system('clear')
+	os.system('echo loading...')
+
+	try:
+		out = subprocess.check_output(['c++','-std=c++11', '-o','run', fn,'-lpthread'])
+	except:
+		return
+
+	os.system('clear')
+	os.system('./run')
+
+
+def loop():
+	global c_file
+	while True:
+		n_file = open(fn, "r").read()
+
+		if(n_file != c_file):
+			compile_and_run()
+			c_file = n_file
+
+		time.sleep(1)
+
+def key_listener():
+	key = ""
+	while True:
+		key = raw_input()
+		if(key == 'r'):
+			compile_and_run()
+		#os.system('clear')
+	
+t1 = Thread(target = loop)
+t2 = Thread(target = key_listener)
+
+if os.path.exists(fn):
+	t1.start()
+	t2.start()
